31be0ceb4702fded3af2745e0a07736b5cb0460f
Ролан Логинов <rolanlog@mail.ru>
2019-11-30 02:56:33 +0300

    Создан метод для вызова методов и два конструктора. Первый - принимающий логин и пароль, вызывающий функцию авторизации. Второй - принимающий токен.

--- src/API.cs
+++ src/API.cs
@@ -1,4 +1,8 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace Monopoly
 {
@@ -8,7 +12,30 @@ namespace Monopoly
 
         public Api(string email, string password)
         {
+            _token = CallMethod("auth.signin", new Dictionary<string, string>
+            {
+                ["email"] = email,
+                ["password"] = password
+            })["access_token"].ToObject<string>();
+        }
 
+        public Api(string token)
+        {
+            _token = token;
+        }
+
+        private JToken CallMethod(string methodName, IDictionary<string, string> parameters)
+        {
+            JToken result;
+
+            using (var web = new WebClient())
+            {
+                result = JToken.Parse(Encoding.UTF8.GetString(web.UploadValues($"https://monopoly-one.com/api/{methodName}", parameters.ToNameValueCollection())));
+            }
+
+            if (result["code"].ToObject<int>() != 0) throw new Exception(result["description"].ToObject<string>());
+
+            return result["data"];
         }
     }
 }

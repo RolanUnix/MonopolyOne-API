904fde8b4dddecbbf672504a18309ab3e0bf6aed
Ролан Логинов <rolanlog@mail.ru>
2019-11-30 02:50:35 +0300

    Создан класс рассширений с методом перевода IDictionary в NameValueCollection

---
+++ src/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Monopoly
+{
+    public static class Extensions
+    {
+        public static NameValueCollection ToNameValueCollection<TKey, TValue>(this IDictionary<TKey, TValue> dict)
+        {
+            var nameValueCollection = new NameValueCollection();
+
+            foreach (var kvp in dict)
+            {
+                string value = null;
+                if (kvp.Value != null)
+                    value = kvp.Value.ToString();
+
+                nameValueCollection.Add(kvp.Key.ToString(), value);
+            }
+
+            return nameValueCollection;
+        }
+    }
+}

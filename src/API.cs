f852d87903abea3ca23f312fc235b92341b55923
Ролан Логинов <rolanlog@mail.ru>
2019-11-30 02:42:29 +0300

    Добавьте файлы проекта.

---
+++ src/API.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Monopoly
+{
+    public class Api
+    {
+        private readonly string _token;
+
+        public Api(string email, string password)
+        {
+
+        }
+    }
+}
---
+++ tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Tests
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}

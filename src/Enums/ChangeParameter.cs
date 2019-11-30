95eea5f7a79992e94da5c5069b19fe3b5d6332af
Ролан Логинов <rolanlog@mail.ru>
2019-11-30 03:55:33 +0300

    Добавлено новое перечисление - тип изменяемого параметра лобби

---
+++ src/Enums/ChangeParameter.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Monopoly.Enums
+{
+    public enum ChangeParameter
+    {
+        [Description("private")]
+        Private,
+
+        [Description("autostart")]
+        AutoStart,
+
+        [Description("maxplayers")]
+        MaxPlayers,
+
+        [Description("game_timers")]
+        Timer,
+
+        [Description("br_corner")]
+        AngularField
+    }
+}

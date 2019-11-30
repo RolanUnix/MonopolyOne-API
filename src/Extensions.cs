120f7a6421581d2a75d2a245105048b566e9c4dd
Ролан Логинов <rolanlog@mail.ru>
2019-11-30 03:51:17 +0300

    Добавлен новый метод расширения. - геттер описания параметра перечисления

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 
 namespace Monopoly
 {
@@ -19,6 +22,20 @@ namespace Monopoly
             }
 
             return nameValueCollection;
+        }
+
+        public static string GetEnumDescription(Enum value)
+        {
+            var fi = value.GetType().GetField(value.ToString());
+
+            var attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            if (attributes != null && attributes.Any())
+            {
+                return attributes.First().Description;
+            }
+
+            return value.ToString();
         }
     }
 }
